d7fa1072e70a0b7365314f7a37371127dbefc099
Diego Andrés da Silva Briceño <diegobriceno13@gmail.com>
2021-12-20 19:41:32 +0000

    Ecrã para reportar avistamento (Incompleto)

---
+++ Seaker/app/src/main/java/com/example/seaker/ReportSightingActivity.java
@@ -0,0 +1,15 @@
+package com.example.seaker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportSightingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_sighting);
+
+    }
+}
