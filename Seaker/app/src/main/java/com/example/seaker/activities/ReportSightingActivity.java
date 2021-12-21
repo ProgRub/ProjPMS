f673e6739600f7f10e0fd297f367e13433968219
Diego Andrés da Silva Briceño <diegobriceno13@gmail.com>
2021-12-21 23:25:28 +0000

    Sighting Information Element (Incomplete)

--- Seaker/app/src/main/java/com/example/seaker/activities/ReportSightingActivity.java
+++ Seaker/app/src/main/java/com/example/seaker/activities/ReportSightingActivity.java
@@ -1,6 +1,13 @@
 package com.example.seaker.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.seaker.R;
 
@@ -11,5 +18,19 @@ public class ReportSightingActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_sighting);
         SetButtonOnClickNextActivity(R.id.buttonBack,TeamMemberHomeActivity.class);
+    }
+
+    public void selectSpecie(View view) {
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.sighting_information_box, null);
+
+        if(view.getId() == R.id.blue_whale_btn) {
+            TextView textView = (TextView) v.findViewById(R.id.title);
+            textView.setText("Blue Whale Sighting");
+        }
+
+        LinearLayout insertPoint = (LinearLayout) findViewById(R.id.sightingsInformations);
+        insertPoint.addView(v);
+
     }
 }
