ba99cc0d3c57de46099d94a8d916a926af1e3511
Diego Andrés da Silva Briceño <diegobriceno13@gmail.com>
2021-12-21 12:32:11 +0000

    Layouts changes

--- Seaker/app/src/main/java/com/example/seaker/ChooseRoleActivity.java
+++ Seaker/app/src/main/java/com/example/seaker/ChooseRoleActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ChooseRoleActivity extends AppCompatActivity {
 
@@ -13,21 +14,21 @@ public class ChooseRoleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_role);
-        ((Button) findViewById(R.id.buttonTeamMember)).setOnClickListener(new View.OnClickListener() {
+        ((ImageButton) findViewById(R.id.buttonTeamMember)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), TeamMemberHomeActivity.class);
                 startActivityForResult(myIntent, 0);
             }
 
         });
-        ((Button) findViewById(R.id.buttonCompanyManager)).setOnClickListener(new View.OnClickListener() {
+        ((ImageButton) findViewById(R.id.buttonCompanyManager)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), CompanyManagerHomeActivity.class);
                 startActivityForResult(myIntent, 0);
             }
 
         });
-        ((Button) findViewById(R.id.buttonAdministrator)).setOnClickListener(new View.OnClickListener() {
+        ((ImageButton) findViewById(R.id.buttonAdministrator)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent myIntent = new Intent(view.getContext(), AdminHomeActivity.class);
                 startActivityForResult(myIntent, 0);
--- Seaker/app/src/main/java/com/example/seaker/TeamMemberHomeActivity.java
+++ Seaker/app/src/main/java/com/example/seaker/TeamMemberHomeActivity.java
@@ -2,7 +2,10 @@ package com.example.seaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TeamMemberHomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class TeamMemberHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_member_home);
+        ((ImageButton) findViewById(R.id.buttonNewSightingReport)).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), ReportSightingActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
