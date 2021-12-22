76f65a93c1ac1f915ef6ba2793bb9eb2584ddb73
Ruben Rodrigues <ruben2000@gmail.com>
2021-12-22 14:23:51 +0000

    Report sighting fragment em ve de activity

--- Seaker/app/src/main/java/com/example/seaker/fragments/ReportSightingFragment.java
+++ Seaker/app/src/main/java/com/example/seaker/fragments/ReportSightingFragment.java
@@ -2,20 +2,20 @@ package com.example.seaker.fragments;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.seaker.MainActivity;
 import com.example.seaker.R;
 
+public class ReportSightingFragment extends BaseFragment {
 
-public class TeamMemberHomeFragment extends BaseFragment {
-
-
-    public TeamMemberHomeFragment() {
+    public ReportSightingFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,10 +25,9 @@ public class TeamMemberHomeFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_team_member_home, container, false);
-        SetButtonOnClickNextFragment(R.id.buttonNewSightingReport,new ReportSightingFragment(),view);
-        SetButtonOnClickNextFragment(R.id.buttonReportedSightingsTeamMember,new ChooseRoleFragment(),view);
-        SetButtonOnClickNextFragment(R.id.buttonLogoutTeamMember,new ChooseRoleFragment(),view);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_report_sighting, container, false);
+        SetButtonOnClickNextFragment(R.id.buttonBack,new TeamMemberHomeFragment(),view);
         return view;
     }
 }
