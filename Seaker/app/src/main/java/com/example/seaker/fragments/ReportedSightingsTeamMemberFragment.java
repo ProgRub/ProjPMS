c1f38935bc2f96f9d12b68ca8b4ab7363f1041b6
Diego Andrés da Silva Briceño <diegobriceno13@gmail.com>
2021-12-28 13:06:48 +0000

    Verificação se já passaram 24h desde o registo do avistamento. Botão para editar ficar oculto após 24h.

--- Seaker/app/src/main/java/com/example/seaker/fragments/ReportedSightingsTeamMemberFragment.java
+++ Seaker/app/src/main/java/com/example/seaker/fragments/ReportedSightingsTeamMemberFragment.java
@@ -2,10 +2,14 @@ package com.example.seaker.fragments;
 
 import android.content.Context;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.seaker.R;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReportedSightingsTeamMemberFragment extends BaseFragment {
 
@@ -29,6 +37,7 @@ public class ReportedSightingsTeamMemberFragment extends BaseFragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,15 +49,16 @@ public class ReportedSightingsTeamMemberFragment extends BaseFragment {
         otherSightings = (LinearLayout) view.findViewById(R.id.other_sightings);
 
         //Para testar inserção:
-        addSightingToView(4, true, "12/1/2021", "16:21", "Blue Whale", "Diego Briceño");
-        addSightingToView(2, false, "14/2/2021", "10:56", "Sperm Whale", "Rúben Rodrigues");
-        addSightingToView(1, false, "15/7/2021", "14:11", "Bottlenose Dolphin, Sperm Whale", "Sílvia Fernandes");
-        addSightingToView(3, true, "16/9/2021", "19:59", "Bottlenose Dolphin, Fin Whale", "Pedro Campos");
+        addSightingToView(4, true, "27/12/2021", "13:05", "Blue Whale", "Diego Briceño");
+        addSightingToView(2, false, "14/2/2021", "10:08", "Sperm Whale", "Rúben Rodrigues");
+        addSightingToView(1, false, "3/10/2021", "9:11", "Bottlenose Dolphin, Sperm Whale", "Sílvia Fernandes");
+        addSightingToView(3, true, "16/11/2021", "19:59", "Bottlenose Dolphin, Fin Whale", "Pedro Campos");
 
         return view;
     }
 
     //Função para adicionar um reported_sighting_box ao ecrã - recebe como parâmetros os dados do sighting:
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void addSightingToView(int sighting_id, boolean submitted, String sighting_date, String sighting_time, String species, String team_member_name ){
         LayoutInflater vi = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View v = vi.inflate(R.layout.reported_sighting_box, null);
@@ -71,10 +81,63 @@ public class ReportedSightingsTeamMemberFragment extends BaseFragment {
         sightingSpecies.setText(species);
         reportedBy.setText(team_member_name);
 
-        if(!submitted) recentSightings.addView(v);
-        else{
+        if(submitted){
             notSubmitted.setVisibility(View.INVISIBLE);
+        }
+
+        //Verificar se já passaram 24h:
+        String dateTimeString = sighting_date +" "+sighting_time;
+        String format = getLocalDateTimeFormatterString(sighting_date, sighting_time);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+        LocalDateTime dateTime = LocalDateTime.parse(dateTimeString, formatter);
+        LocalDateTime after24h = dateTime.plusHours(24);
+
+        if(after24h.isAfter(LocalDateTime.now())){ //menos de 24h
+           recentSightings.addView(v);
+        }else{ //já não pode editar - mais de 24h
+            editSightingBtn.setVisibility(View.INVISIBLE);
             otherSightings.addView(v);
         }
+    }
+
+
+    private String getLocalDateTimeFormatterString(String sighting_date, String sighting_time) {
+        String[] date_parts = sighting_date.split("/");
+        String day = date_parts[0];
+        String month = date_parts[1];
+        String[] time_parts = sighting_time.split(":");
+        String hours = time_parts[0];
+        String minutes = time_parts[1];
+
+        String format = "";
+        DateTimeFormatter formatter;
+        if(day.length() == 1){
+            format +="d/";
+        }else if(day.length() ==2){
+            format +="dd/";
+        }
+
+        if(month.length() == 1){
+            format +="M/";
+        }else if(month.length() ==2){
+            format +="MM/";
+        }
+
+        format+="yyyy ";
+
+        if(hours.length() == 1){
+            format +="H:";
+        }else if(hours.length() ==2){
+            format +="HH:";
+        }
+
+        if(minutes.length() == 1){
+            format +="m";
+        }else if(minutes.length() ==2){
+            format +="mm";
+        }
+
+        return format;
     }
 }
