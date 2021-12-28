576d796800a18d8cc390f52289a79089b810c688
Ruben Rodrigues <ruben2000@gmail.com>
2021-12-28 22:16:10 +0000

    Design dos ecrãs de login

---
+++ Seaker/app/src/main/java/com/example/seaker/fragments/LoginManagerAdminFragment.java
@@ -0,0 +1,33 @@
+package com.example.seaker.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.seaker.R;
+
+public class LoginManagerAdminFragment extends Fragment {
+
+
+    public LoginManagerAdminFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login_manager_admin, container, false);
+        return view;
+    }
+}
---
+++ Seaker/app/src/main/java/com/example/seaker/fragments/LoginTeamMemberFragment.java
@@ -0,0 +1,31 @@
+package com.example.seaker.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.seaker.R;
+
+public class LoginTeamMemberFragment extends Fragment {
+
+    public LoginTeamMemberFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login_team_member, container, false);
+        return view;
+    }
+}
